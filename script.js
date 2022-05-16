c94c20a33e6490085f2bbe211d987ad9556572e3
ciisiq <cii_3@hotmail.com>
2022-05-16 17:50:34 +0100

    84 switching the player

--- script.js
+++ script.js
@@ -9,13 +9,17 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 const current0El = document.getElementById('current--0');
 const current1EL = document.getElementById('current--1');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 
 //starting the condition
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 //Rolling the dice functionality
 btnRoll.addEventListener('click', function () {
@@ -30,8 +34,16 @@ btnRoll.addEventListener('click', function () {
     // add dice to the current score
     // currentScore = currentScore + dice;
     currentScore += dice;
-    current0El.textContent = currentScore; // CHANGE LATER;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+
+    // current0El.textContent = currentScore; // CHANGE LATER;
   } else {
     //switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
