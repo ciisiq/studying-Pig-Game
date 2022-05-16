8999a3b880bd35f8d5e3d742ed2feab2fd47d2e3
ciisiq <cii_3@hotmail.com>
2022-05-16 18:18:29 +0100

    85 holding current score

--- script.js
+++ script.js
@@ -21,11 +21,18 @@ const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  currentScore = 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
 //Rolling the dice functionality
 btnRoll.addEventListener('click', function () {
   // 1. Genarating a random dice roll
   const dice = Math.trunc(Math.random() * 6) + 1;
-  console.log(dice);
   //2.Display the dice
   diceEl.classList.remove('hidden');
   diceEl.src = `dice-${dice}.png`;
@@ -40,10 +47,27 @@ btnRoll.addEventListener('click', function () {
     // current0El.textContent = currentScore; // CHANGE LATER;
   } else {
     //switch to next player
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    currentScore = 0;
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
+    switchPlayer();
   }
+});
+
+btnHold.addEventListener('click', function () {
+  // 1. add current score to active player
+  //  scores[1] = scores[1] + currentScore
+  scores[activePlayer] += currentScore;
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
+  // 2. Check if player's score is >= 100
+  if (scores[activePlayer] >= 100) {
+    //Finish the game
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.add('player--winner');
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.remove('player--active');
+  }
+
+  //switch to the next player
+  switchPlayer();
 });
