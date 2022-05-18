d62b8c10e05b54703d9ec6c5246c1953c76c71f4
ciisiq <cii_3@hotmail.com>
2022-05-18 17:11:08 +0100

    86 resetting the game and finished it

--- script.js
+++ script.js
@@ -12,16 +12,6 @@ const current1EL = document.getElementById('current--1');
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
 
-//starting the condition
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
-
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
-
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
   activePlayer = activePlayer === 0 ? 1 : 0;
@@ -29,6 +19,29 @@ const switchPlayer = function () {
   player0El.classList.toggle('player--active');
   player1El.classList.toggle('player--active');
 };
+
+let scores, currentScore, activePlayer, playing;
+
+const init = function () {
+  //starting the condition
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1EL.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+
+init();
 
 //Rolling the dice functionality
 btnRoll.addEventListener('click', function () {
@@ -78,3 +91,6 @@ btnHold.addEventListener('click', function () {
     }
   }
 });
+
+//JS call the function and reset all the game when clicked
+btnNew.addEventListener('click', init);
